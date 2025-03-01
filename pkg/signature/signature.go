bc7ed5905fb25483125d15e793c04017e4f369c3
Ardin Fardiansyah <ardfarde@gmail.com>
2025-03-02 01:19:44 +0700

    update auth

--- pkg/signature/signature.go
+++ pkg/signature/signature.go
@@ -11,12 +11,12 @@ import (
 )
 
 // GenerateSNAPAsymmetricSignature generates a signature for the given client secret and timestamp using the private key
-func GenerateSNAPAsymmetricSignature(clientSecret string, timestamp time.Time, privateKey rsa.PrivateKey) (string, error) {
+func GenerateSNAPAsymmetricSignature(clientSecret string, timestamp time.Time, privateKey *rsa.PrivateKey) (string, error) {
 	data := fmt.Sprintf("%s|%s", clientSecret, timestamp.Format(time.RFC3339))
 
 	// generate signature using PKCS1 private key
 	dataHash := sha256.Sum256([]byte(data))
-	signature, err := rsa.SignPKCS1v15(rand.Reader, &privateKey, crypto.SHA256, dataHash[:])
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, dataHash[:])
 	if err != nil {
 		return "", fmt.Errorf("failed to populate signature, err: %s", err)
 	}
