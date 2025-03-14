91ced1fe374a3bad2e38b7ed22057fa73cee7a7a
Reza Putra <agipreza@gmail.com>
2025-03-14 23:02:08 +0700

    add signature auth client

--- authentication.go
+++ authentication.go
@@ -1,8 +1,13 @@
 package doitpay
 
 import (
+	"bytes"
 	"context"
+	"github.com/pkg/errors"
+	"io"
+	"net/http"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -17,113 +22,182 @@ import (
 
 // AccessToken represents an access token with expiration time
 type AccessToken struct {
-    AccessToken string
-    ExpiresAt   time.Time
-    TokenType   string
+	AccessToken string
+	ExpiresAt   time.Time
+	TokenType   string
 }
+
+var (
+	ErrorBearerTokenNotFound    = errors.New("mandatory header bearer token not found")
+	ErrorAccessTokenInvalid     = errors.New("access token invalid")
+	ErrorTimestampNotFound      = errors.New("mandatory header timestamp not found")
+	ErrorExternalIdNotFound     = errors.New("mandatory header external id not found")
+	ErrorPartnerIdNotFound      = errors.New("mandatory header partner id not found")
+	ErrorSignatureNotFound      = errors.New("mandatory header signature not found")
+	ErrorPartnerIdNotRegistered = errors.New("ValidateSignature: partner id not registered")
+	ErrorSignatureInvalid       = errors.New("ValidateSignature: invalid signature")
+)
 
 // Custom auth implementation
 type DoitpayAuth struct {
-    config ClientConfig
-    accessToken *AccessToken
-    mu sync.Mutex
+	config      ClientConfig
+	accessToken *AccessToken
+	mu          sync.Mutex
 }
 
 func NewDoitpayAuth(config ClientConfig) *DoitpayAuth {
-    return &DoitpayAuth{config: config}
+	return &DoitpayAuth{config: config}
 }
 
 // GetAccessToken retrieves an access token from the authentication service
 func (a *DoitpayAuth) GetAccessToken(ctx context.Context) (string, error) {
-    a.mu.Lock()
-    defer a.mu.Unlock()
+	a.mu.Lock()
+	defer a.mu.Unlock()
 
-    if a.accessToken == nil || a.accessToken.ExpiresAt.Before(time.Now()) {
-    
-        timestamp := time.Now().UTC()
-        signature, err := signature.GenerateSNAPAsymmetricSignature(a.config.ClientSecret, timestamp, a.config.PrivateKey)
-        if err != nil {
-            return "", err
-        }
+	if a.accessToken == nil || a.accessToken.ExpiresAt.Before(time.Now()) {
 
-        transport := httptransport.New(a.config.Host, a.config.BasePath, []string{"https", "http"})
-        authService := client.New(transport, strfmt.Default).Authentication
+		timestamp := time.Now().UTC()
+		signature, err := signature.GenerateSNAPAsymmetricSignature(a.config.ClientSecret, timestamp, a.config.PrivateKey)
+		if err != nil {
+			return "", err
+		}
 
-        resp, err := authService.AccessToken(&authentication.AccessTokenParams{
-            XTIMESTAMP: timestamp.Format(time.RFC3339),
-            XSIGNATURE: signature,
-            XCLIENTKEY: a.config.ClientSecret,
-            Request: &models.PartnerAccessTokenRequest{
-                GrantType: "client_credentials",
-            },
-        })
+		transport := httptransport.New(a.config.Host, a.config.BasePath, []string{"https", "http"})
+		authService := client.New(transport, strfmt.Default).Authentication
 
-        if err != nil {
-            return "", err
-        }
+		resp, err := authService.AccessToken(&authentication.AccessTokenParams{
+			XTIMESTAMP: timestamp.Format(time.RFC3339),
+			XSIGNATURE: signature,
+			XCLIENTKEY: a.config.ClientSecret,
+			Request: &models.PartnerAccessTokenRequest{
+				GrantType: "client_credentials",
+			},
+		})
 
-        expiresIn, err := strconv.Atoi(resp.GetPayload().ExpiresIn)
-        if err != nil {
-            return "", err
-        }
+		if err != nil {
+			return "", err
+		}
 
-        expiresAt := time.Now().Add(time.Duration(expiresIn) * time.Second)
-        a.accessToken = &AccessToken{
-            AccessToken: resp.GetPayload().AccessToken,
-            ExpiresAt:   expiresAt,
-            TokenType:   resp.GetPayload().TokenType,
-        }
-        return a.accessToken.AccessToken, nil
-    }
-    return a.accessToken.AccessToken, nil
+		expiresIn, err := strconv.Atoi(resp.GetPayload().ExpiresIn)
+		if err != nil {
+			return "", err
+		}
+
+		expiresAt := time.Now().Add(time.Duration(expiresIn) * time.Second)
+		a.accessToken = &AccessToken{
+			AccessToken: resp.GetPayload().AccessToken,
+			ExpiresAt:   expiresAt,
+			TokenType:   resp.GetPayload().TokenType,
+		}
+		return a.accessToken.AccessToken, nil
+	}
+	return a.accessToken.AccessToken, nil
 }
 
 // AuthenticateRequest authenticates a request with the access token
 func (a *DoitpayAuth) AuthenticateRequest(req runtime.ClientRequest, reg strfmt.Registry) error {
-    timestamp := time.Now().UTC()
-    
-    // Set required headers
-    headers := map[string]string{
-        "X-CLIENT-KEY": a.config.ClientSecret,
-        "X-TIMESTAMP":  timestamp.Format(time.RFC3339),
-        "X-PARTNER-ID": a.config.PartnerID,
-        "X-SIGNATURE":  "",
-    }
+	timestamp := time.Now().UTC()
 
-    accessToken, err := a.GetAccessToken(context.Background())
-    if err != nil {
-        return err
-    }
+	// Set required headers
+	headers := map[string]string{
+		"X-CLIENT-KEY": a.config.ClientSecret,
+		"X-TIMESTAMP":  timestamp.Format(time.RFC3339),
+		"X-PARTNER-ID": a.config.PartnerID,
+		"X-SIGNATURE":  "",
+	}
 
-    endpoint := req.GetPath()
-    if params := req.GetQueryParams(); len(params) > 0 {
-        endpoint = endpoint + "?" + params.Encode()
-    }
+	accessToken, err := a.GetAccessToken(context.Background())
+	if err != nil {
+		return err
+	}
 
-    // Generate signature
-    signature, err := signature.GenerateSNAPSymmetricSignature(
-        req.GetMethod(),
-        endpoint,
-        accessToken,
-        a.config.ClientSecret,
-        timestamp,
-        req.GetBody(),
-    )
+	endpoint := req.GetPath()
+	if params := req.GetQueryParams(); len(params) > 0 {
+		endpoint = endpoint + "?" + params.Encode()
+	}
 
-    if err != nil {
-        return err
-    }
+	// Generate signature
+	signature, err := signature.GenerateSNAPSymmetricSignature(
+		req.GetMethod(),
+		endpoint,
+		accessToken,
+		a.config.ClientSecret,
+		timestamp,
+		req.GetBody(),
+	)
 
-    headers["X-SIGNATURE"] = signature
-    headers["Authorization"] = "Bearer " + accessToken
+	if err != nil {
+		return err
+	}
 
-    // Set all headers
-    for key, value := range headers {
-        if err := req.SetHeaderParam(key, value); err != nil {
-            return err
-        }
-    }
+	headers["X-SIGNATURE"] = signature
+	headers["Authorization"] = "Bearer " + accessToken
 
-    return nil
+	// Set all headers
+	for key, value := range headers {
+		if err := req.SetHeaderParam(key, value); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
+func (a *DoitpayAuth) ValidateSignature(ctx context.Context, req *http.Request) error {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return errors.Wrap(err, "ValidateSignature: failed to read request body")
+	}
+
+	req.Body = io.NopCloser(bytes.NewBuffer(body))
+
+	bearerToken := req.Header.Get("Authorization")
+	if bearerToken == "" {
+		return ErrorBearerTokenNotFound
+	}
+
+	if len(strings.Split(bearerToken, " ")) < 2 {
+		return ErrorAccessTokenInvalid
+	}
+
+	if strings.Split(bearerToken, " ")[0] != "Bearer" {
+		return ErrorAccessTokenInvalid
+	}
+
+	headerTimestamp := req.Header.Get("X-TIMESTAMP")
+	if headerTimestamp == "" {
+		return ErrorTimestampNotFound
+	}
+
+	externalId := req.Header.Get("X-EXTERNAL-ID")
+	if externalId == "" {
+		return ErrorExternalIdNotFound
+	}
+
+	partnerId := req.Header.Get("X-PARTNER-ID")
+	if partnerId == "" {
+		return ErrorPartnerIdNotFound
+	}
+
+	headerSignatureKey := req.Header.Get("X-SIGNATURE")
+	if headerSignatureKey == "" {
+		return ErrorSignatureNotFound
+	}
+
+	timestamp, err := time.Parse(time.RFC3339, headerTimestamp)
+	if err != nil {
+		return ErrorTimestampNotFound
+	}
+
+	token := strings.Split(bearerToken, " ")[1]
+	expectedSignature, err := signature.GenerateSNAPSymmetricSignature(req.Method, req.URL.Path, token,
+		a.config.ClientSecret, timestamp, body)
+	if err != nil {
+		return ErrorSignatureInvalid
+	}
+
+	if expectedSignature != headerSignatureKey {
+		return ErrorSignatureInvalid
+	}
+	return nil
+}
--- pkg/signature/signature.go
+++ pkg/signature/signature.go
@@ -8,9 +8,12 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/sha512"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/pem"
 	"fmt"
+	"github.com/pkg/errors"
 	"net/http"
 	"strings"
 	"time"
@@ -62,3 +65,53 @@ func GenerateSNAPSymmetricSignature(httpMethod, endpointUrl, accessToken, client
 	return base64.StdEncoding.EncodeToString(hm.Sum(nil)), nil
 }
 
+func ParsePrivateKey(private []byte) (*rsa.PrivateKey, error) {
+	privatePem, _ := pem.Decode(private)
+	if privatePem == nil {
+		return nil, errors.New("failed parse private key")
+	}
+	var privatePemBytes []byte
+	if privatePem.Type != "RSA PRIVATE KEY" && privatePem.Type != "ENCRYPTED PRIVATE KEY" && privatePem.Type != "PRIVATE KEY" {
+		return nil, fmt.Errorf("RSA private key is of the wrong type: %s", privatePem.Type)
+	}
+	privatePemBytes = privatePem.Bytes
+
+	var parsedKey interface{}
+	parsedKey, err := x509.ParsePKCS1PrivateKey(privatePemBytes)
+	if err != nil {
+		if parsedKey, err = x509.ParsePKCS8PrivateKey(privatePemBytes); err != nil {
+			return nil, fmt.Errorf("unable to parse RSA private key, generating a temp one: %s", err.Error())
+		}
+	}
+
+	var privateKey *rsa.PrivateKey
+	var ok bool
+	privateKey, ok = parsedKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("unable to parse RSA private key, generating a temp one : %s", err.Error())
+	}
+
+	return privateKey, nil
+}
+
+func ParsePublicKey(pub []byte) (*rsa.PublicKey, error) {
+	pubPem, _ := pem.Decode(pub)
+	if pubPem == nil {
+		return nil, errors.New("failed decode public key")
+	}
+	if pubPem.Type != "PUBLIC KEY" {
+		return nil, fmt.Errorf("RSA public key is of the wrong type, Pem Type :%s", pubPem.Type)
+	}
+
+	parsedKey, err := x509.ParsePKIXPublicKey(pubPem.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse RSA public key, generating a temp one: %s", err.Error())
+	}
+
+	pubKey, ok := parsedKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("unable to parse RSA public key")
+	}
+
+	return pubKey, nil
+}
